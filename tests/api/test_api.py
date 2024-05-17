6ce94d03c75201bffcaeb308b760b11e4a904897
mnevzorov2002 <mnevzorov2002@ukr.net>
2024-05-17 16:39:14 +0300

    Formed structure

---
+++ tests/api/test_api.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = "Sergii"
+        self.second_name = "Butenko"
+
+
+@pytest.fixture
+def user():
+    yield User()
+
+
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+
+def test_name(user):
+    assert user.name == "Sergii"
+
+
+def test_second_name(user):
+    assert user.second_name == "Butenko"
