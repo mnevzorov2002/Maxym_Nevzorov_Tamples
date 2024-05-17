6f3ef2de91b02dba56f64707da7fae59ca9384da
mnevzorov2002 <mnevzorov2002@ukr.net>
2024-05-17 19:56:19 +0300

    Former structure

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,26 +1,17 @@
 import pytest
 
 
-class User:
-
-    def __init__(self) -> None:
-        self.name = "Sergii"
-        self.second_name = "Butenko"
-
-
-@pytest.fixture
-def user():
-    yield User()
-
-
+@pytest.mark.change
 def test_remove_name(user):
     user.name = ''
     assert user.name == ''
 
 
+@pytest.mark.check
 def test_name(user):
     assert user.name == "Sergii"
 
 
+@pytest.mark.check
 def test_second_name(user):
     assert user.second_name == "Butenko"
